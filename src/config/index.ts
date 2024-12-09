8765f728d9452a4eeacde1f571ee0716bf6a4d96
Binod Gautam <gautambinod629@gmail.com>
2024-12-09 14:56:44 +0545

    add docker production file

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,8 @@
-import dotenv from 'dotenv';
-dotenv.config();
+import { config } from 'dotenv';
+import path from 'path';
+config({
+  path: path.join(__dirname, `../../.env.${process.env.NODE_ENV || 'dev'}`),
+});
 const { PORT, NODE_ENV, DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_NAME } = process.env;
 
 export const Config = {
